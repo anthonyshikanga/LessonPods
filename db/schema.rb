d9ec6de70a4d29c8dcf93f3bdf72e3ecb8d0f6a6
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-30 12:59:35 +0300

    nested routeng added for episodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828093956) do
+ActiveRecord::Schema.define(version: 20170830094729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "episodes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "podcast_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["podcast_id"], name: "index_episodes_on_podcast_id"
+  end
 
   create_table "podcasts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 20170828093956) do
     t.index ["reset_password_token"], name: "index_podcasts_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "episodes", "podcasts"
 end
