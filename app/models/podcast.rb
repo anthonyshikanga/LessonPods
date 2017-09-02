beac5bc399fa2853c3377561268871faecc5a600
anthonyshikanga <anthonyshikanga@gmail.com>
2017-09-02 11:02:36 +0300

    modified podcast.rb with has many through

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -3,7 +3,9 @@ class Podcast < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :episodes
+  has_many :episodes, through: :hearts
+
+  has_many :hearts, dependent: :destroy
 
   has_attached_file :thumbnail, :styles => { :large => "1000x1000#", :medium => "550x550#" }
 validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
