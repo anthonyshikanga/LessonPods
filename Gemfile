a8d5c6000249c2e5cf31333a67410c6bc92d6d93
anthonyshikanga <anthonyshikanga@gmail.com>
2017-09-02 19:57:09 +0300

    installed commontator gem

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 ruby '2.2.3'
 
-
+gem 'commontator'
 gem 'paperclip', '~> 4.2.1'
 gem 'devise'
 gem 'rails', '~> 5.1.3'
@@ -18,6 +18,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'twitter'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -3,9 +3,7 @@ class Podcast < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :episodes, through: :hearts
-
-  has_many :hearts, dependent: :destroy
+  has_many :episodes
 
   has_attached_file :thumbnail, :styles => { :large => "1000x1000#", :medium => "550x550#" }
 validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
